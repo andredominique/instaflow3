46756027a45127c74f11aeaba1f2314adc767134
andredominique <andre@andredominique.com>
2025-08-31 08:54:08 +0000

    Restore left pane (sidebar) width in CaptionsView to previous value; document reduced width for future reference

--- CaptionsView.swift
+++ CaptionsView.swift
@@ -155,7 +155,9 @@ struct CaptionsView: View {
             .padding(.horizontal, 12)
             .padding(.bottom, 8)
         }
-    .frame(width: 120)
+    // Restored sidebar width (previously reduced for smaller window)
+    // To allow smaller resizing again, set width: 120
+    .frame(width: 320)
         .padding(8)
         .background(Color.gray.opacity(0.06))
     }
